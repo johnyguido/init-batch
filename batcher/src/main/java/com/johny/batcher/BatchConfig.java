ae1c8f30166fedb13ad1148d1bc812629e4671dc
Johny Alencar <johnyguido@hotmail.com.br>
2023-06-27 21:54:46 -0300

    16. Prática: Criação de um step chunk-based

--- batcher/src/main/java/com/johny/batcher/BatchConfig.java
+++ batcher/src/main/java/com/johny/batcher/BatchConfig.java
@@ -2,19 +2,19 @@ package com.johny.batcher;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableBatchProcessing
 @Configuration
@@ -26,32 +26,39 @@ public class BatchConfig {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job imprimeOlaJob() {
+    public Job imprimeParImparJob() {
         return jobBuilderFactory
-                .get("imprimeOlaJob")
-                .start(imprimeOlaStep())
+                .get("imprimeParImparJob")
+                .start(imprimeParImparStep())
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
 
-    public Step imprimeOlaStep() {
+    public Step imprimeParImparStep() {
         return stepBuilderFactory
-                .get("imprimeOlaStep")
-                .tasklet(imprimeOlaTasklet(null))
+                .get("imprimeParImparStep")
+                .<Integer, String>chunk(1)
+                .reader(contaAteDezReader())
+                .processor(parOuImparProcessor())
+                .writer(imprimeWriter())
                 .build();
 
     }
 
-    @Bean
-    @StepScope
-    public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']})") String nome) {
-        return new Tasklet() {
-            @Override
-            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                System.out.println(String.format("Processo Batch executado por %s", nome));
-                return RepeatStatus.FINISHED;
-            }
-        };
+    private ItemWriter<String> imprimeWriter() {
+        return itens -> itens.forEach(System.out::println);
     }
+
+    private FunctionItemProcessor<Integer, String> parOuImparProcessor() {
+        return new FunctionItemProcessor<Integer, String>(item ->
+                item % 2 == 0 ? String.format("Item %s é par", item) : String.format("Item %s é impar", item)
+        );
+    }
+
+    private IteratorItemReader<Integer> contaAteDezReader() {
+        List<Integer> numerosDeUmAteDez = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        return new IteratorItemReader<Integer>(numerosDeUmAteDez.iterator());
+    }
+
 }
 
